e4944cc19ffce6d4e6f25833185db8df07d0ab25
drdolgosheev <*Dima1975>
2021-03-26 19:09:45 +0300

    drug validator API added, security changed

--- src/main/java/com/hse/organizer/rest/DrugRestControllerV1.java
+++ src/main/java/com/hse/organizer/rest/DrugRestControllerV1.java
@@ -1,13 +1,12 @@
 package com.hse.organizer.rest;
 
 import com.hse.organizer.model.Drug;
+import com.hse.organizer.modules.implementation.DrugCodeValidatorImplementation;
 import com.hse.organizer.service.DrugService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +19,17 @@ public class DrugRestControllerV1 {
     public DrugRestControllerV1(DrugService drugService) {
         this.drugService = drugService;
     }
-    @GetMapping(value="get/all")
-    public ResponseEntity<List<Drug>> getAllDrugs(){
+
+    @GetMapping(value = "get/all")
+    public ResponseEntity<List<Drug>> getAllDrugs() {
         List<Drug> result = drugService.getAllDrugs();
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "validate/{code}")
+    public ResponseEntity<Boolean> validateDrug(@PathVariable(name = "code") String code) {
+        DrugCodeValidatorImplementation validator = new DrugCodeValidatorImplementation();
+        Boolean result = validator.validate(code);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
